3c8829373665c04696b02a9374d905199718615e
timohani <sd3059272@gmail.com>
2026-02-05 21:42:47 +0500

    custom item

--- src/main/java/com/timohani/tutorialmod/TutorialMod.java
+++ src/main/java/com/timohani/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.timohani.tutorialmod;
 
+import com.timohani.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,6 +19,6 @@ public class TutorialMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 	}
 }
