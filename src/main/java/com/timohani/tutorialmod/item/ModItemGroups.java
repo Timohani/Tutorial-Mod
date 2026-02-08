fc2d1000e3294b3b71616b7883e13f7fb641e0b6
timohani <sd3059272@gmail.com>
2026-02-08 20:02:02 +0500

    Custom Advanced Item

--- src/main/java/com/timohani/tutorialmod/item/ModItemGroups.java
+++ src/main/java/com/timohani/tutorialmod/item/ModItemGroups.java
@@ -20,6 +20,8 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.PINK_GARNET);
                         entries.add(ModItems.RAW_PINK_GARNET);
+
+                        entries.add(ModItems.CHISEL);
                     })
                     .build());
 
