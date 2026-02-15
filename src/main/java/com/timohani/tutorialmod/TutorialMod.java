e2d7c5ec23ebb6385bdb5bb915dd48648f90d8c8
timohani <sd3059272@gmail.com>
2026-02-15 17:00:48 +0500

    Explaining Events | #23

--- src/main/java/com/timohani/tutorialmod/TutorialMod.java
+++ src/main/java/com/timohani/tutorialmod/TutorialMod.java
@@ -6,29 +6,46 @@ import com.timohani.tutorialmod.item.ModItemGroups;
 import com.timohani.tutorialmod.item.ModItems;
 import com.timohani.tutorialmod.util.HammerUsageEvent;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "tutorialmod";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
 
-		ModItemGroups.registerItemGroups();
+        ModItemGroups.registerItemGroups();
 
-		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 
-		ModDataComponentTypes.registerDataComponentTypes();
+        ModDataComponentTypes.registerDataComponentTypes();
 
-		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 20000);
+        FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 20000);
 
-		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
-	}
+        PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (entity instanceof SheepEntity sheepEntity && !world.isClient()) {
+                if (player.getMainHandStack().getItem() == Items.END_ROD) {
+                    player.sendMessage(Text.literal("THE PLAYER JUST HIT A SHEEP WITH AN END ROD!"));
+                    player.getMainHandStack().decrement(1);
+                    sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 600, 6));
+                }
+            }
+
+            return ActionResult.PASS;
+        });
+    }
 }
