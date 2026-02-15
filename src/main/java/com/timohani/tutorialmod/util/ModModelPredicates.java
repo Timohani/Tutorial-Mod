263b7ce54e6e8af05e52afdc01ce3264fe4994cf
timohani <sd3059272@gmail.com>
2026-02-15 16:08:49 +0500

    Model Predicates | #21

---
+++ src/main/java/com/timohani/tutorialmod/util/ModModelPredicates.java
@@ -0,0 +1,14 @@
+package com.timohani.tutorialmod.util;
+
+import com.timohani.tutorialmod.TutorialMod;
+import com.timohani.tutorialmod.component.ModDataComponentTypes;
+import com.timohani.tutorialmod.item.ModItems;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.util.Identifier;
+
+public class ModModelPredicates {
+    public static void registerModelPredicates() {
+        ModelPredicateProviderRegistry.register(ModItems.CHISEL, Identifier.of(TutorialMod.MOD_ID, "used"),
+                (stack, world, entity, seed) -> stack.get(ModDataComponentTypes.COORDINATES) != null ? 1f : 0f);
+    }
+}
