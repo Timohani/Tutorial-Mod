6dd4ae92d4e736a4a1a5fc093518ca7fa9ab7652
timohani <sd3059272@gmail.com>
2026-03-13 14:54:54 +0500

    Entity Variants | #40

--- src/main/java/com/timohani/tutorialmod/entity/client/MantisRender.java
+++ src/main/java/com/timohani/tutorialmod/entity/client/MantisRender.java
@@ -1,21 +1,35 @@
 package com.timohani.tutorialmod.entity.client;
 
+import com.google.common.collect.Maps;
 import com.timohani.tutorialmod.TutorialMod;
 import com.timohani.tutorialmod.entity.custom.MantisEntity;
+import com.timohani.tutorialmod.entity.custom.MantisVariant;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.Map;
 
 public class MantisRender extends MobEntityRenderer<MantisEntity, MantisModel<MantisEntity>> {
+    private static final Map<MantisVariant, Identifier> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(MantisVariant.class), map -> {
+                map.put(MantisVariant.DEFAULT,
+                        Identifier.of(TutorialMod.MOD_ID, "textures/entity/mantis/mantis.png"));
+                map.put(MantisVariant.ORCHID,
+                        Identifier.of(TutorialMod.MOD_ID, "textures/entity/mantis/mantis_orchid.png"));
+            });
+
+
     public MantisRender(EntityRendererFactory.Context context) {
         super(context, new MantisModel<>(context.getPart(MantisModel.MANTIS)), 0.75f);
     }
 
     @Override
     public Identifier getTexture(MantisEntity entity) {
-        return Identifier.of(TutorialMod.MOD_ID, "textures/entity/mantis/mantis.png");
+        return LOCATION_BY_VARIANT.get(entity.getVariant());
     }
 
     @Override
