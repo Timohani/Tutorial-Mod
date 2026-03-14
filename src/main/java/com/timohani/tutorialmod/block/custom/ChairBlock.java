9ef0ed939f231f8b4d863c9344417515f50d7806
timohani <sd3059272@gmail.com>
2026-03-14 20:09:37 +0500

    45. update

--- src/main/java/com/timohani/tutorialmod/block/custom/ChairBlock.java
+++ src/main/java/com/timohani/tutorialmod/block/custom/ChairBlock.java
@@ -7,35 +7,22 @@ import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
-import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
 
 public class ChairBlock extends HorizontalFacingBlock {
     public static final MapCodec<ChairBlock> CODEC = createCodec(ChairBlock::new);
+    private static final VoxelShape SHAPE = Block.createCuboidShape(3.0, 0.0, 3.0, 13.0, 16.0, 13.0);
 
     public ChairBlock(Settings settings) {
         super(settings);
     }
 
-    public VoxelShape makeShape(){
-        VoxelShape shape = VoxelShapes.empty();
-        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0.1875, 0, 0.6875, 0.3125, 0.4375, 0.8125), BooleanBiFunction.OR);
-        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0.6875, 0, 0.6875, 0.8125, 0.4375, 0.8125), BooleanBiFunction.OR);
-        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0.6875, 0, 0.1875, 0.8125, 0.4375, 0.3125), BooleanBiFunction.OR);
-        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0.1875, 0, 0.1875, 0.3125, 0.4375, 0.3125), BooleanBiFunction.OR);
-        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0.18125, 0.4375, 0.15625, 0.81875, 0.5, 0.8125), BooleanBiFunction.OR);
-        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0.1875, 0.5, 0.71875, 0.8125, 1.1875, 0.8125), BooleanBiFunction.OR);
-
-        return shape;
-    }
-
     @Override
     protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return makeShape();
+        return SHAPE;
     }
 
     @Override
