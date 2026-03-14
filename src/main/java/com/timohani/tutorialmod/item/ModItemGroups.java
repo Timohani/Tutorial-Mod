c4bcc954e5d65e9840caaa0da7dedb50e91cc22f
timohani <sd3059272@gmail.com>
2026-03-14 20:34:16 +0500

    Animated Textures | #47

--- src/main/java/com/timohani/tutorialmod/item/ModItemGroups.java
+++ src/main/java/com/timohani/tutorialmod/item/ModItemGroups.java
@@ -53,6 +53,7 @@ public class ModItemGroups {
                         entries.add(ModItems.MANTIS_SPAWN_EGG);
 
                         entries.add(ModItems.TOMAHAWK);
+                        entries.add(ModItems.SPECTRE_STAFF);
                     })
                     .build());
 
